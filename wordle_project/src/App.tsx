fa1710e49efac609e4159eebbace1d5a1c34602f
ehsan-golbar <1382ehg@gmail.com>
2024-09-03 01:45:24 +0330

    install tailwind and make the structure

--- wordle_project/src/App.tsx
+++ wordle_project/src/App.tsx
@@ -1,10 +1,22 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import Board from "./components/Board";
+import Keyboard from "./components/Keyboard";
+import Header from "./components/Header";
 // import './App.css'
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <Header></Header>
+        <Board></Board>
+
+        <Keyboard></Keyboard>
+      </div>
+    </>
+  );
 }
 
 export default App;
